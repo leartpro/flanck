5c84fef169ff3d6713cfc5e1f0c7097ddea3beba
Tony <65500763+bit-burger@users.noreply.github.com>
2023-04-21 20:14:16 +0200

    implement Stack methods

--- src/Stack.h
+++ src/Stack.h
@@ -7,9 +7,26 @@
 using namespace std;
 struct Stack {
     explicit Stack(vector<bool> stack) {
-        this->stack = std::move(stack);
+        this->data_ = std::move(stack);
     }
-    vector<bool> stack;
+    vector<bool> data_;
 
+    bool endsWith(Stack other) {
+        if(other.data_.empty() && data_.empty()) {
+            return true;
+        }
+        if(other.data_.size() > data_.size()) {
+            return false;
+        }
+        return std::equal(data_.end() - int(other.data_.size()), data_.end(), other.data_.end());
+    }
+
+    void push(Stack other) {
+        data_.insert(data_.end(), other.data_.begin(), other.data_.end() );
+    }
+
+    void remove(const Stack& howMuch) {
+        data_.resize(data_.size() - howMuch.data_.size());
+    }
 };
 #endif //FLANCK_STACK_H
