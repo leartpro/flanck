051b43ac5f1781a1462b0c685b3c6f9fc0f6c1f7
Tony <65500763+bit-burger@users.noreply.github.com>
2023-04-21 23:46:29 +0200

    continue interpreter

--- src/Interpreter.cpp
+++ src/Interpreter.cpp
@@ -1,5 +1,24 @@
 #include "Interpreter.h"
 
-void Interpreter::interpret(vector<Statement>* programStack) {
-
+void Interpreter::interpret(vector<Statement>* statements) {
+    bool stackChanged;
+    do {
+        stackChanged = false;
+        for (const Statement& statement: *statements) {
+            for(int i = 0; i < statement.conditions.size(); i++) {
+                if (!stacks_[i].endsWith(statement.conditions[i])) {
+                    goto next_statement;
+                }
+            }
+            // because no Statements are added that don't read and write, if the
+            stackChanged = true;
+            for(int i = 0; i < statement.conditions.size(); i++) {
+                stacks_[i].remove(statement.conditions[i]);
+            }
+            for(int i = 0; i < statement.instructions.size(); i++) {
+                stacks_[i].push(statement.instructions[i]);
+            }
+            next_statement:;
+        }
+    } while (stackChanged);
 }
--- src/Interpreter.h
+++ src/Interpreter.h
@@ -4,15 +4,41 @@
 #include <string>
 #include <utility>
 #include <unordered_map>
-#include "Token.h"
+#include <set>
 #include "Statement.h"
+#include "Parser.h"
+
+class InterpreterObserver
+{
+public:
+    InterpreterObserver() = default;
+    inline bool inputAsk();
+};
+
+struct StackOption {
+    bool onlyAdd;
+    bool onlyTake;
+    bool shouldNotifyRead;
+};
+
+struct InterpreterOptions {
+    const int readCount;
+    const bool notifyBytes;
+    const int maxCount;
+    const bool resetCountAfterSuccessfulInputAsk;
+    const unordered_map<int, StackOption>;
+
+};
 
 class Interpreter
 {
 public:
-    Interpreter() = default;
-    void interpret(vector<Statement>* programStack);
+    explicit Interpreter(Parser& parser) : parser_(parser) {};
+    void interpret(vector<Statement>* statements);
+    vector<Statement> getStacks();
 private:
+    Parser &parser_;
     vector<Stack> stacks_;
+    InterpreterOptions options_;
 };
 #endif //PLANCK_INTERPRETER_H
